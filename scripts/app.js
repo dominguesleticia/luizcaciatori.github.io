84bfcd7a20773575e773fdfd3bedbada7a71658b
Letícia Domingues <domingues.leticia@hotmail.com>
2016-04-14 16:51:16 -0300

    Link para popup após click em título do evento

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,7 @@ function dataSave() {
 	alert('Registro Salvo com Sucesso!')	
 	history.go(-1);
 }
+
+function newPopup(evento){
+	alert('Nome do Evento: '+evento+'\nData da Realização: 30/04/2016');
+}
